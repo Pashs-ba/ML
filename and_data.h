1599f7b85f09dd2d37e009ef81ed44fa8add02eb
Pashs-ba <pashs.ba@gmail.com>
2020-09-27 16:47:15 +0300

    read_from_mnist

--- and_data.h
+++
@@ -1,9 +0,0 @@
-#include <vector>
-using namespace std;
-vector<vector<double>> inp = {{1, 0},
-                              {0, 1},
-                              {1, 1},
-                              {0, 0}
-};
-
-vector<int> ans = {1, 1, 1, 0};
--- main.cpp
+++ main.cpp
@@ -1,55 +1,26 @@
 #include <vector>
 #include "Neuron.h"
-#include "sum_nums_data.h"
+#include <string>
 #include <iostream>
 #include <cmath>
+#include "read/mnist_reader.h"
 
 using namespace std;
 
 int main(){
-
-    Neuron and_perceptron({7.39695, 7.39674}, .5);
-
-//     Learning
-//    for (int i = 0; i<10000; i++){
-//        for (int j = 0; j<ans.size(); j++){
-//            and_perceptron.learning(inp[j], ans[j]);
-//        }
-//        cout << i << ' ' << and_perceptron.Weights[0] << ' ' << and_perceptron.Weights[1] << endl;
-//    }
-    vector<Neuron> layer;
-    for (int i = 0; i<10; i++){
-        Neuron tmp({.5, .5}, .5);
-        layer.emplace_back(tmp);
+    string file = __FILE__;
+    file.resize(file.size()-8);
+    cout<<file;
+    string s = "lol.txt";
+    file += "lol.txt";
+    MNISTReader mnist(
+                    //file+("train-labels.idx1-ubyte.gz"),
+            file,
+                    file+("train-images.idx3-ubyte.gz"));
+    mnist.read();
+    for(auto i: mnist.mLabelValues){
+        cout<<i<<endl;
     }
-//    Learning
-    for (int i = 0; i<1000000; i++) {
-        for (int j = 0; j<ans.size(); j++) {
-            vector<int> ans_for_layer;
-            ans_for_layer.resize(10);
-            ans_for_layer[ans[j]] = 1;
-            for (int k = 0; k<layer.size(); k++){
-                layer[k].learning(inp[j], ans_for_layer[k]);
-//                cout<<layer[k].Weights[0]<<' '<<layer[k].Weights[1]<<' '<<ans_for_layer[k]<<endl;
-            }
-//            cout<<endl<<endl;
-        }
-
-    }
-
-    for (int j = 0; j<inp.size(); j++){
-        double max_ans = 0;
-        int max_val = 0;
-        for (int i = 0; i<layer.size(); i++ ){
-//        cout<<layer[i].output({1, 2})<<endl;
-            if (layer[i].output(inp[j]) > max_ans){
-                max_ans = layer[i].output(inp[j]);
-                max_val = i;
-            }
-        }
-        cout<<max_val<<' '<<ans[j]<<endl;
-    }
-
-//    cout << round(and_perceptron.output({1, 1}));
+    return 0;
 
 }
